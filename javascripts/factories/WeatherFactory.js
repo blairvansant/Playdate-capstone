dc43677d0a0ce74a0c045d8093c5ffdfe8e8de88
blairvansant <blairbot@gmail.com>
2016-12-17 14:12:02 -0600

    deploy

--- javascripts/factories/WeatherFactory.js
+++ javascripts/factories/WeatherFactory.js
@@ -1,21 +1,21 @@
-"use strict";
+// "use strict";
 
-playdate.factory("WeatherFactory", function($q, $http, WEATHER_CONFIG){
+// playdate.factory("WeatherFactory", function($q, $http, WEATHER_CONFIG){
 	
-	var getWeatherIcon = function(userId){
-		return $q((resolve, reject)=>{
-			$http.get(`http://api.wunderground.com/api/${WEATHER_CONFIG.apiKey}/forecast10day/q/TN/Nashville.json`)
-			.success(function(response){
-				let weatherIcons = [];
-				Object.keys(response).forEach(function(key){
-					response[key].id = key;
-					weatherIcons.push(response[key]);
-				});
-				resolve(weatherIcons);
-				console.log("weatherIcons", weatherIcons);
-			})
-			.error(function(errorResponse){
-				reject(errorResponse);
-			});
-		});
-	};
+// 	var getWeatherIcon = function(userId){
+// 		return $q((resolve, reject)=>{
+// 			$http.get(`http://api.wunderground.com/api/${WEATHER_CONFIG.apiKey}/forecast10day/q/TN/Nashville.json`)
+// 			.success(function(response){
+// 				let weatherIcons = [];
+// 				Object.keys(response).forEach(function(key){
+// 					response[key].id = key;
+// 					weatherIcons.push(response[key]);
+// 				});
+// 				resolve(weatherIcons);
+// 				console.log("weatherIcons", weatherIcons);
+// 			})
+// 			.error(function(errorResponse){
+// 				reject(errorResponse);
+// 			});
+// 		});
+// 	};
